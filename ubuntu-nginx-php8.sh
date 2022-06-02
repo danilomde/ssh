0b2903b58a21631b09b2785fcdb24e304c50982c
Danilo Zeni <danilomde@gmail.com>
2022-06-02 17:17:38 -0300

    .

--- ubuntu-nginx-php8.sh
+++ ubuntu-nginx-php8.sh
@@ -4,7 +4,7 @@
 SITE_NAME=listapix.com.br
 
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository ppa:ondrej/php -y && sudo add-apt-repository ppa:ondrej/nginx -y
 sudo apt-get update 
