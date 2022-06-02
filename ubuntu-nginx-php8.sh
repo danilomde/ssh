850f0b49d9c61b1a7140753b13e942393359fad1
Danilo Zeni <danilomde@gmail.com>
2022-06-02 20:47:56 -0300

    Ok

--- ubuntu-nginx-php8.sh
+++ ubuntu-nginx-php8.sh
@@ -1,48 +1,73 @@
 #!/bin/bash
 
 #Edite aqui o site que deseja consigurar
-SITE_NAME=listapix.com.br
+DNS=listapix.com.br
+
+SITE_NAME=${1:-${DNS}}
+#SITE_NAME=${1:-listapix.com.br}
+
+echo -e "Creating server for \n \t\t ${SITE_NAME}"
 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository ppa:ondrej/php -y && sudo add-apt-repository ppa:ondrej/nginx -y
 sudo apt-get update 
-
 sudo apt install php-fpm wget unzip zip git php-common php-mysql php-cgi php-mbstring php-curl php-gd php-xml php-xmlrpc php-pear php-zip curl nginx -y
 
 sudo usermod -aG sudo $USER
 
 CONFIG_FILE="${SITE_NAME}.conf"
+SERVER_DOMAINS="${SITE_NAME} www.${SITE_NAME}"
+APP_PATH="/var/www/${SITE_NAME}"
 
-cat >> $CONFIG_FILE << EOF
+sudo mkdir -p "${APP_PATH}/public"
+
+if [ ! -f "${APP_PATH}/public/index.php" ]; then
+	echo -e "<?php phpinfo(); " |sudo tee ${APP_PATH}/public/index.php
+fi
+
+if [ ! -f "${APP_PATH}/public/index.php" ]; then
+	echo -e "\nThe file '${APP_PATH}/public/index.php not exists'.\n"
+	exit 1;
+fi
+
+cat > $CONFIG_FILE << EOF
+## INICIO Nginx conf php
 server {
     listen 80;
-    listen [::]:80;
-
-    server_name  _ ;
-
-    root /var/www/${SITE_NAME}/public;
+    #    listen 443 ssl;
+    server_name ${SERVER_DOMAINS};
+    root ${APP_PATH}/public;
+    ## Redirecionar para https
+    #return 301 https://$host$request_uri;
 
     index index.php index.html index.htm;
-    error_log  /var/log/nginx/error.log;
-    access_log /var/log/nginx/access.log;
+    #    ssl_certificate /etc/nginx/ssl_certs/cert.pem;
+    #    ssl_certificate_key /etc/nginx/ssl_certs/cert.key;
 
     location / {
-        try_files $uri $uri/ /index.php?$args;
+        #    autoindex on;
+        try_files \$uri \$uri/ /index.php?\$query_string;
     }
-    
-    location ~ \.php$ {
+
+    #    location / {
+    #        try_files $uri $uri/ =404;
+    #    }
+
+    location ~ \.php\$ {
         include snippets/fastcgi-php.conf;
-        #change to php version instaled
         fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
+        fastcgi_buffers 16 16k;
+        fastcgi_buffer_size 32k;
     }
-    
-    location ~* \.(js|css|png|jpg|jpeg|gif|ico|svg)$ {
-        expires max;
-        log_not_found off;
+
+    location ~ /\.ht {
+        deny all;
     }
+
 }
+## FIM Nginx conf php
 EOF
 
 if [ -f /etc/nginx/sites-enabled/default ];
@@ -51,22 +76,18 @@ fi
 
 #sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/${SITE_NAME}
 
-sudo cp ./${SITE_NAME} /etc/nginx/sites-available/${SITE_NAME}
-sudo ln -s /etc/nginx/sites-available/${SITE_NAME} /etc/nginx/sites-enabled/
+sudo mv ./${CONFIG_FILE} /etc/nginx/sites-available/${CONFIG_FILE}
+sudo ln -s /etc/nginx/sites-available/${CONFIG_FILE} /etc/nginx/sites-enabled/
 
-sudo mkdir -p "/var/www/${SITE_NAME}/public"
+sudo mkdir -p "${APP_PATH}/public"
 
 sudo chown -R $USER:www-data /var/www/${SITE_NAME}/
-sudo chmod -R 750 /var/www/${SITE_NAME}/
-sudo chmod -R g+s /var/www/${SITE_NAME}/
-
-cd /var/www/${SITE_NAME}
-
+sudo chmod -R 750 "${APP_PATH}"
+sudo chmod -R g+s "${APP_PATH}"
 
 sudo systemctl enable nginx
 
 sudo service nginx restart
-
 
 sudo wget https://getcomposer.org/download/latest-2.2.x/composer.phar -O /usr/bin/composer
 sudo chmod +x /usr/bin/composer
